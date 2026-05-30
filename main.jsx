adee2d7954de7fce7f3b72a7de1b7eb18d9cf6e0
Sudha <gayatripuvvada9@gmail.com>
2026-05-30 17:05:13 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,17 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // Make sure this contains your Tailwind directives
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import ErrorBoundary from "./ErrorBoundary";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </BrowserRouter>
+  </React.StrictMode>
 );
