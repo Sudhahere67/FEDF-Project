cfed20e81dbe6c2127f417b2461a2c3342352821
Sudhahere67 <sudhagayathri@klh.edu.in>
2026-06-12 10:27:25 +0530

    Delete NoticeContext.jsx

--- NoticeContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const NoticeContext = createContext();
-
-export const useNoticeContext = () => {
-  return useContext(NoticeContext);
-};
